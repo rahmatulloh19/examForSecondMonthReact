dbbdeb0aceab9bb95927bce5c52757871da4414c
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-02-13 01:37:16 +0500

    add home and books decorations

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -14,7 +14,9 @@ export const Categories = ({ isBooks }) => {
 	return (
 		<section className="categories">
 			<div className="container">
-				<h2 className="text-[32px] leading-[48px] mb-3 text-center">{t("categories.title")}</h2>
+				<h2 className="text-[32px] leading-[48px] mb-3 text-center dark:text-[#C9AC8C] text-black">
+					{t("categories.title")}
+				</h2>
 				<ul className="flex justify-center gap-x-[54px] mb-[30px]">
 					{categories &&
 						categories.map((item) => {
@@ -37,7 +39,9 @@ export const Categories = ({ isBooks }) => {
 							}
 							return (
 								<li key={item.id}>
-									<label>
+									<label
+										className="cursor-pointer
+									">
 										<input
 											className="visually-hidden categories__input"
 											type="radio"
@@ -55,16 +59,16 @@ export const Categories = ({ isBooks }) => {
 				{!isBooks ? (
 					<ul className="grid grid-cols-4 gap-x-5 gap-y-6">
 						{array &&
-							array.map((item) => (
-								<li className="rounded-[22px] bg-[#F5F5F5] dark:bg-[#1E1E1E]">
+							array.map((item, index) => (
+								<li className="rounded-[22px] bg-[#F5F5F5] dark:bg-[#1E1E1E]" key={index}>
 									<img
-										className="rounded-t-[22px] mb-3"
+										className="rounded-t-[22px]"
 										src="https://placehold.co/295x244"
 										alt=""
 										width={295}
 										height={244}
 									/>
-									<div className="min-h-[129px] p-4 pt-3 dark:text-[#C9AC8C] text-black">
+									<div className="author__item min-h-[141px] p-4 pt-3 dark:text-[#C9AC8C] text-black rounded-b-[22px]">
 										<h3 className="text-2xl font-bold leading-9 mb-1.5">Abdulla Avloniy</h3>
 										<div className="flex justify-between">
 											<span className="text-justify text-[#00000099] leading-6 dark:text-[#FFFFFF99]">
@@ -81,8 +85,8 @@ export const Categories = ({ isBooks }) => {
 				) : (
 					<ul className="grid grid-cols-6 gap-x-5 gap-y-6">
 						{array &&
-							array.map((item) => (
-								<li>
+							array.map((item, index) => (
+								<li key={index}>
 									<img
 										className="rounded-[15px] mb-3"
 										src="https://placehold.co/190x283"
@@ -90,9 +94,11 @@ export const Categories = ({ isBooks }) => {
 										height={283}
 										alt=""
 									/>
-									<h3 className="text-lg font-bold leading-9 mb-1.5">Dunyoning ishlari</h3>
+									<h3 className="text-lg font-bold leading-9 mb-1.5 dark:text-[#C9AC8C] text-black">
+										Dunyoning ishlari
+									</h3>
 									<span className="text-justify text-[#00000099] leading-6 dark:text-[#FFFFFF99]">
-										1878-1934
+										O’tkir Hoshimov
 									</span>
 								</li>
 							))}
